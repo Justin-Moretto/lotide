bd42a15042e275fac06cbfffc9ab1c11ed8fef3b
Justin Moretto <JustinMorettoMTL@gmail.com>
2020-10-28 19:54:14 +0000

    letterPosition.js, takes strings, outputs object which lists posisions where each letter in the string occurs

---
+++ letterPosition.js
@@ -0,0 +1,17 @@
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let [letter] = sentence[i];
+    if (results[letter]) { //is this a letter we're tracking?
+      results[letter].push((i));// is so, track it's position
+    } else {  //if not
+      results[letter] = []; //create an array to track if first
+      results[letter].push(i);
+    }
+  }
+  return results;
+};
+
+//test
+console.log(letterPositions("hello"));
