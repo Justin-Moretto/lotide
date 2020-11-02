a780bdc11ff58f24afad49eabaaf3b0ba4f8c778
Justin Moretto <JustinMorettoMTL@gmail.com>
2020-11-02 22:08:06 +0000

    refactored asserArraysEqual and it's test to use modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let pass = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (!(arr1[i] === arr2[i])) { //if the values at equal indexes of both arrays are not the same
-      pass = false;
-      break;
-    }
-  }
-  //console.log(pass);
-  return pass;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2) === true) {
@@ -18,5 +8,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [5, 5, 5]);
+module.exports = assertArraysEqual;
