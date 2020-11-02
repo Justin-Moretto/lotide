79baa0efa97c3bdaee538508b124efd96545bc10
Justin Moretto <JustinMorettoMTL@gmail.com>
2020-11-02 22:19:56 +0000

    refactored middle.js and it's test to use modules

--- middle.js
+++ middle.js
@@ -1,22 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  let pass = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (!(arr1[i] === arr2[i])) { //if the values at equal indexes of both arrays are not the same
-      pass = false;
-      break;
-    }
-  }
-  //console.log(pass);
-  return pass;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✔️ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`✖️ Assertion Failed: ${arr1} === ${arr2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 // ACTUAL FUNCTION
 const middle = function(array) {
@@ -33,7 +17,4 @@ const middle = function(array) {
   return output;
 };
 
-// TEST CODE
-//console.log(middle([1, 2, 3, 4, 5]));
-//console.log(middle([1, 2, 3, 4, 5, 6]));
-//console.log(middle([1, 2]));
+module.exports = middle;
